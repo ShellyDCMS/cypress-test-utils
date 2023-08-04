beff0182b54bc1167bf3bc1c9651b959c86478aa
shelly_goldblit <shelly_goldblit@dell.com>
2023-08-04 18:42:09 +0300

    more tests

--- src/cypress-helper-spec.cy.ts
+++ src/cypress-helper-spec.cy.ts
@@ -84,6 +84,13 @@ describe("cypress helper tests", () => {
     expect(await get.inputValue("name-input")).to.eq("shelly");
   });
 
+  it("should click button", async () => {
+    when.clear("name-input");
+    when.type("name-input", "shelly");
+    when.click("submit");
+    expect(await get.inputValue("name-input")).to.eq("John");
+  });
+
   it("should get initial text input", async () => {
     expect(await get.inputValue("last-name-input")).to.eq("Doe");
   });
@@ -116,5 +123,19 @@ describe("cypress helper tests", () => {
     when.check("checkbox", 2);
     when.uncheck("checkbox", 2);
     expect(get.elementByTestId("checkbox", 2).should("not.be.checked"));
+  });
+
+  it("should get computed style", async () => {
+    expect((await get.elementsComputedStyle("button")).backgroundColor).to.eq(
+      "rgb(255, 0, 0)"
+    );
+  });
+
+  it("should get image source", async () => {
+    expect(await get.elementsAttribute("image", "src")).to.eq("w3schools.jpg");
+  });
+
+  it("should get element's text", async () => {
+    expect(await get.elementsText("header")).to.eq("My First Heading");
   });
 });
