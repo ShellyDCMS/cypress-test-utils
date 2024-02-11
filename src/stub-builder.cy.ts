be0fbca548d71c56167fb35346b7fe1fbc62245d
shelly_goldblit <shelly_goldblit@dell.com>
2024-02-11 07:51:29 +0200

    add tests for inherited class

--- src/stub-builder.cy.ts
+++ src/stub-builder.cy.ts
@@ -168,12 +168,15 @@ describe("stub builder tests", () => {
 
   describe("stubbing class", () => {
     class MyClass {
-      constructor(input: number) {}
+      constructor(input: number) {
+        throw new Error("Should not be called");
+      }
       func(input: number, text: string) {
         console.log(text);
         return input;
       }
       property: number = 3;
+      optionalProperty?: number;
       get getter(): number {
         return this.property;
       }
@@ -188,6 +191,133 @@ describe("stub builder tests", () => {
         return await Promise.resolve(9);
       }
     }
+
+    class MyInheritedClass extends MyClass {
+      constructor() {
+        super(5);
+        throw new Error("Should not be called");
+      }
+    }
+
+    describe("Given inherited class", () => {
+      it("should assert stub async function calls", async () => {
+        const mockMyInheritedClass =
+          given.stubbedInstance<MyInheritedClass>(MyInheritedClass);
+        mockMyInheritedClass.asynFunc.returns(Promise.resolve(7));
+        mockMyInheritedClass.propertyFunc(3);
+        then(
+          get.assertableStub(mockMyInheritedClass.asynFunc)
+        ).shouldHaveBeenCalledWith(3);
+      });
+
+      it("should assert stub async function calls using overrides", async () => {
+        const stub = given.stub().returns(Promise.resolve(7));
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          asynFunc: stub
+        });
+        mockMyInheritedClass.propertyFunc(3);
+        then(
+          get.assertableStub(mockMyInheritedClass.asynFunc)
+        ).shouldHaveBeenCalledWith(3);
+      });
+
+      it("should stub async function", async () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.asynFunc.returns(Promise.resolve(7));
+        mockMyInheritedClass.propertyFunc(3);
+        expect(await mockMyInheritedClass.asynFunc()).to.eq(7);
+      });
+
+      it("should stub async function using overrides", async () => {
+        const stub = given.stub().returns(Promise.resolve(7));
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          asynFunc: stub
+        });
+        mockMyInheritedClass.propertyFunc(3);
+        expect(await mockMyInheritedClass.asynFunc()).to.eq(7);
+      });
+
+      it("should stub async property function", async () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        await mockMyInheritedClass.asyncPropertyFunc(3);
+        then(
+          get.assertableStub(mockMyInheritedClass.asyncPropertyFunc)
+        ).shouldHaveBeenCalledWith(3);
+      });
+
+      it("should override class property Function", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.propertyFunc.returns(7);
+        expect(mockMyInheritedClass.propertyFunc(3)).to.eq(7);
+      });
+
+      it("should stub class property function", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.propertyFunc(3);
+        then(
+          get.assertableStub(mockMyInheritedClass.propertyFunc)
+        ).shouldHaveBeenCalledWith(3);
+      });
+
+      it("should override class property", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          property: 5
+        });
+        expect(mockMyInheritedClass.property).to.eq(5);
+      });
+
+      it("should override class getter", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          getter: 5
+        });
+        expect(mockMyInheritedClass.getter).to.eq(5);
+      });
+
+      it("should stub class setter with override", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          property: 8
+        });
+        expect(mockMyInheritedClass.property).to.eq(8);
+      });
+
+      it("should assert stub class setter calls", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.setter = 5;
+        then(
+          get.assertableStub(mockMyInheritedClass.setter)
+        ).shouldHaveBeenCalledWith(5);
+      });
+
+      it("should allow setting properties", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          property: 8
+        });
+        mockMyInheritedClass.property = 5;
+        expect(mockMyInheritedClass.property).to.eq(8);
+      });
+
+      it("should allow setting optional properties", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass, {
+          optionalProperty: 8
+        });
+        mockMyInheritedClass.optionalProperty = 5;
+        expect(mockMyInheritedClass.optionalProperty).to.eq(8);
+      });
+
+      it("should stub class", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.func(5, "whatever");
+        then(
+          get.assertableStub(mockMyInheritedClass.func)
+        ).shouldHaveBeenCalledWith(5, "whatever");
+      });
+
+      it("should stub class function return value", () => {
+        const mockMyInheritedClass = given.stubbedInstance(MyInheritedClass);
+        mockMyInheritedClass.func.returns(7);
+        expect(mockMyInheritedClass.func(5, "whatever")).to.eq(7);
+      });
+    });
 
     it("should assert stub async function calls", async () => {
       const mockMyClass = given.stubbedInstance<MyClass>(MyClass);
@@ -268,6 +398,14 @@ describe("stub builder tests", () => {
       const mockMyClass = given.stubbedInstance(MyClass, { property: 8 });
       mockMyClass.property = 5;
       expect(mockMyClass.property).to.eq(8);
+    });
+
+    it("should allow setting optional properties", () => {
+      const mockMyClass = given.stubbedInstance(MyClass, {
+        optionalProperty: 8
+      });
+      mockMyClass.optionalProperty = 5;
+      expect(mockMyClass.optionalProperty).to.eq(8);
     });
 
     it("should stub class", () => {
