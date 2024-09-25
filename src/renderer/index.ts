953ef50e8ce3fa7ed6a319ac95b2d8991cdf919c
ShellyDCMS <shelly.goldblit@gmail.com>
2024-09-25 18:20:37 +0300

    refactor renderer

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,26 +1,45 @@
 import type { Type } from "@angular/core";
-import type { ReactWebComponent } from "@lit/react";
-import { spread } from "@open-wc/lit-helpers";
 import type { MountConfig } from "cypress/angular";
-import { type TemplateResult } from "lit";
-import { templateContent } from "lit-html/directives/template-content.js";
-import { html, unsafeStatic } from "lit/static-html.js";
-import type { Attributes } from "react";
-import ReactHtmlParser from "react-html-parser";
-import { CypressHelper } from "..";
+import { CypressHelper } from "../";
 import { CypressAngularComponentHelper } from "../angular";
-import { CypressLitComponentHelper } from "../lit";
 import { CypressReactComponentHelper } from "../react";
 
-/**
- *
- */
-export interface FrameworkRenderers {
-  renderLitComponent?: () => void;
-  renderReactComponent?: () => void;
-  renderAngularComponent?: () => void;
+import { spread } from "@open-wc/lit-helpers";
+import type { LitElement, TemplateResult } from "lit";
+import { templateContent } from "lit-html/directives/template-content.js";
+import { html, unsafeStatic } from "lit/static-html.js";
+import ReactHtmlParser from "react-html-parser";
+import { CypressLitComponentHelper } from "../lit";
+
+export interface IRenderer {
+  render: () => void;
 }
+
+export interface getPropsAndchildren {}
+export interface Options {
+  getAngularOptions?: <T>() => {
+    type: Type<T>;
+    config: MountConfig<T>;
+    props?: Partial<T>;
+  };
+  getReactOptions?: <T extends LitElement>() => {
+    // @ts-ignore
+    type: ReactWebComponent<T> | T;
+    // @ts-ignore
+    props?: (Attributes & Partial<ReactWebComponent<T>["props"]>) | null;
+  };
+  getLitOptions?: <T extends LitElement>() => {
+    element?: { new (...args: any[]): LitElement };
+    selector?: string;
+    children?: string;
+    props?: Partial<T>;
+    template?: TemplateResult;
+  };
+}
+
 /**
+ * @classdesc Designed for running the same component tests  for different framework
+ *
  * render a component (in Component Test context) of specific framework (React/Lit/Angular) based on an env variable
  * @example
  * in cypress config file
@@ -43,99 +62,105 @@ export interface FrameworkRenderers {
  *       framework: 'angular',
  *   },
  * ```
- */
-export class Renderer<T> {
-  constructor() {}
+ * @example
+ * ```ts
+ * const children: string = `
+ * <span slot="alert-title">Title</span>
+ * <span slot="alert-details">Alert!!!</span>
+ * `;
+ * const renderFactory: RenderFactory = new RenderFactory({
+ *           getLitOptions: () => ({ element: AlertElement, selector: 'my-alert', props: {type: "inline", status: "danger"}, children }),
+ *           getReactOptions: () => ({ type: MyAlert, props: { type: "inline", status: "danger" , children} }),
+ *       });
+ * const renderer = renderFactory.createRenderer()
+ * renderer.render()
+ * ```
+ *
+ **/
+export class RenderFactory {
+  constructor(private readonly options: Options) {}
 
-  public when = {
-    render: ({
-      renderReactComponent,
-      renderLitComponent,
-      renderAngularComponent
-    }: FrameworkRenderers) => {
-      const helper = new CypressHelper();
-      const framework = helper.get.env("framework");
-      if (framework === "angular" && renderAngularComponent)
-        return renderAngularComponent();
-      if (framework === "react" && renderReactComponent)
-        return renderReactComponent();
-      if (framework === "lit" && renderLitComponent)
-        return renderLitComponent();
-    },
+  /**
+   * @returns IRenderer
+   * @example
+   */
+  public createRenderer(): IRenderer {
+    const helper = new CypressHelper();
+    const framework = helper.get.env("framework");
+    if (framework === "angular" && this.options.getAngularOptions)
+      return {
+        render: () => this.renderAngular(this.options.getAngularOptions!())
+      };
+    if (framework === "react" && this.options.getReactOptions)
+      return {
+        render: () => this.renderReact(this.options.getReactOptions!())
+      };
+    if (framework === "lit" && this.options.getLitOptions)
+      return { render: () => this.renderLit(this.options.getLitOptions!()) };
+    /** TODO: REMOVE hack for testing with missing components */
+    if (this.options.getLitOptions)
+      return { render: () => this.renderLit(this.options.getLitOptions!()) };
+    return { render: () => {} };
+  }
 
-    /**
-     * @example
-     * const renderer = new Renderer<DatagridElement>();
-     * renderer.when.renderLit({ element: DropDownElement, selector: 'ids-dropdown', props: get.props(), children: get.children() });
-     *
-     * @example
-     * const renderer = new Renderer<DatagridElement>();
-     * renderer.when.renderLit({ element: DatagridElement, template });
-     **/
-    renderLit: ({
-      element,
-      selector,
-      props,
-      children = "",
-      template
-    }: {
-      element?: { new (...args: any[]): T };
-      selector?: string;
-      children?: string;
-      props?: Partial<T>;
-      template?: TemplateResult;
-    }) => {
-      const litComponentHelper = new CypressLitComponentHelper();
-      if (template) {
-        litComponentHelper.when.unmount();
-        // @ts-ignore
-        litComponentHelper.when.mount<T>(template, element);
-      } else {
-        const templateResult = html`<${unsafeStatic(selector!)} ${spread(
-          props!
-        )}>${this.get.litChildren(children!)}</${unsafeStatic(selector!)}>`;
-        litComponentHelper.when.unmount();
-        // @ts-ignore
-        litComponentHelper.when.mount<T>(templateResult, element);
-      }
-    },
-    /**
-     * @example
-     *  const renderer = new Renderer<AlertElement>();
-     * renderer.when.renderReact(AlertElement, { ...get.props(), children: get.children() });
-     **/
-    renderReact: (
+  private renderLit<T extends LitElement>({
+    element,
+    selector,
+    props,
+    children = "",
+    template
+  }: {
+    element?: { new (...args: any[]): T };
+    selector?: string;
+    children?: string;
+    props?: Partial<T>;
+    template?: TemplateResult;
+  }) {
+    const litComponentHelper = new CypressLitComponentHelper();
+    if (template) {
+      litComponentHelper.when.unmount();
       // @ts-ignore
-      type: ReactWebComponent<T> | T,
+      litComponentHelper.when.mount<T>(template, element);
+    } else {
+      const templateResult = html`<${unsafeStatic(selector!)} ${spread(
+        props!
+      )}>${this.get.litChildren(children!)}</${unsafeStatic(selector!)}>`;
+      litComponentHelper.when.unmount();
       // @ts-ignore
-      props?: (Attributes & Partial<ReactWebComponent<T>["props"]>) | null
-    ) => {
-      const reactComponentHelper = new CypressReactComponentHelper();
-      reactComponentHelper.when.mount(
-        // @ts-ignore
-        type,
-        props,
-        this.get.reactChildren(props.children)
-      );
-    },
-
-    /**
-     * @example
-     * const testConfig = {
-     *   declarations: [AvatarComponent],
-     * };
-     * const renderer = new Renderer<AvatarComponent>();
-     * renderer.when.renderAngular(AvatarComponent, testConfig get.props());
-     **/
-    renderAngular: (
-      type: Type<T>,
-      config: MountConfig<T>,
-      props?: Partial<T>
-    ) => {
-      const angularComponentHelper = new CypressAngularComponentHelper();
-      angularComponentHelper.when.mount(type, config, props);
+      litComponentHelper.when.mount<T>(templateResult, element);
     }
-  };
+  }
+
+  private renderReact<T extends LitElement>({
+    type,
+    props
+  }: {
+    // @ts-ignore
+    type: ReactWebComponent<T> | T;
+    // @ts-ignore
+    props?: (Attributes & Partial<ReactWebComponent<T>["props"]>) | null;
+  }) {
+    const reactComponentHelper = new CypressReactComponentHelper();
+    reactComponentHelper.when.mount(
+      // @ts-ignore
+      type,
+      props,
+      this.get.reactChildren(props.children)
+    );
+  }
+
+  private renderAngular<T>({
+    type,
+    config,
+    props
+  }: {
+    type: Type<T>;
+    config: MountConfig<T>;
+    props?: Partial<T>;
+  }) {
+    const angularComponentHelper = new CypressAngularComponentHelper();
+    angularComponentHelper.when.mount(type, config, props);
+  }
 
   private get = {
     reactChildren: (children: string): React.ReactElement[] => {
