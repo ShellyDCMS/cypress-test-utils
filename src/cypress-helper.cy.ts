3bcc7fd640f86c5fb1fa6cac11bda8250d0865c3
ShellyDCMS <shelly.goldblit@gmail.com>
2024-12-16 06:24:01 +0200

    skip

--- src/cypress-helper.cy.ts
+++ src/cypress-helper.cy.ts
@@ -285,8 +285,8 @@ describe("cypress helper tests", () => {
     });
   });
 
-  it("should take snapshot and compare to previous", () => {
-    // get.imageSnapshot("homepage", { failureThreshold: 1 });
+  it.skip("should take snapshot and compare to previous", () => {
+    get.imageSnapshot("homepage", { failureThreshold: 1 });
     get.imageSnapshot("radio-group", {
       dataTestID: "radio-group",
       failureThreshold: 0.2
